cbe5d06bc45297fbcc92a2cc1fac1b7d087f7fac
Neha <nehashaikh470@gmail.com>
2023-03-15 22:43:41 +0100

    link bug fix

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import NavHeader from './components/NavHeader';
-import { BrowserRouter } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavHeader from "./components/NavHeader";
+import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "react-scroll-to-top";
-import Footer from './components/Footer';
-
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <main>
       <BrowserRouter>
-      <NavHeader />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
-      {/* <Footer /> */}
-      <ScrollToTop smooth />
+        <NavHeader />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+        <Footer />
+        <ScrollToTop smooth />
       </BrowserRouter>
     </main>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
 export default function Footer() {
-    return (
-        <section id="footer">
-            <footer className="text-center" style={{ backgroundColor: "black"}}>
-                Neha Shaikh |  Berlin, Germany
-            </footer>
-        </section>
-    )
+  return (
+    <section id="footer">
+      <footer className="text-center">
+        <hr></hr>
+        <p>
+          Made with : &nbsp;
+          <img
+            className="icon-footer"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+          />
+          &nbsp;
+          <img
+            className="icon-footer"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+          />
+        </p>
+        <p>Made By : Neha Shaikh</p>
+      </footer>
+    </section>
+  );
 }
